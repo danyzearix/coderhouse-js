e9b9af3733a99c79722b23f9010db2d906bef7be
Dany m <atriasoft.ceo@gmail.com>
2022-04-14 22:45:05 -0500

    Desafio complementario #1

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/* Desafio complementario #1 */
+
+//Contar de 10 en 10 hasta llegar a 100
+
+for (let i = 0; i <= 100; i += 10) {
+  alert(i);
+}
+
+// Contar hasta 10 y mostrar un alert
+
+alert("Relajate, vamos a contar hasta 10")
+
+let i = 1;
+
+while (i < 11) {
+    alert(`Cuenta ${i}`)
+
+    i++;
+
+    if (i === 11) {
+        alert(`Felicidades, ya contaste hasta 10 ¡Estas relajado :)!`);
+
+    }
+
+}
