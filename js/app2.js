074535a6dc0d21a8354fadc5d3fa17f557945751
dsmr <atriasoft.ceo@gmail.com>
2022-06-09 01:45:34 -0500

    Nuevas caracteristicas 2

--- js/app2.js
+++ js/app2.js
@@ -1,33 +1,6 @@
-//Clase constructora
-  class ObjetoCotizador{
-      constructor(label, precio,){
-        this.label = label
-        this.precio =  precio
-      }
-    };
-    
-    //Crear objetos 
-    //Tipo de tecnologia
-    let tecnologiaUno = new ObjetoCotizador ("Nativa", 3000, "alta");
-    let tecnologiaDos = new ObjetoCotizador ("Hibrida", 2500, "media");
-    let tecnologiaTres = new ObjetoCotizador ("PWA", 1500, "baja")
-    
-    //Sistemas operativos nativos
-    let sistemaOperativoUno = new ObjetoCotizador ("Android", 1500, "media");
-    let sistemaOperativoDos = new ObjetoCotizador ("iOS", 2500, "alta");
-    
-    //Sistema hibrido
-    let sistemaHibrido = new ObjetoCotizador ("Android y iOS", 5000, "alta");
-    
-    //Diseño
-    let plantilla = new ObjetoCotizador ("Plantilla", 500, "bajo");
-    let propio = new ObjetoCotizador ("Propio", 1000, "medio");
-    let avanzado = new ObjetoCotizador ("Avanzado a medida", 2700, "alto");
-    
+
     //Carácteristica login
-    let nologin = new ObjetoCotizador ("Sin login", 0, "ninguna");
-    let loginCorreo = new ObjetoCotizador ("Con correo", 200, "baja");
-    let loginRedes = new ObjetoCotizador ("Con redes", 700, "Alta");
+    
     
     //Tipo de app
     let tipoInformativa = new ObjetoCotizador ("Informativa", 200, "baja");
@@ -57,12 +30,6 @@
   
   //Array vacio que recibirá la selección del usuario con método push 
   
-  let arrayVacio = [unIdioma,dosIdiomas,tresIdiomas,cuatroIdiomas];
-  
-  //Boton que inicia el algoritmo
-  document.querySelector("#iniciar");
-  iniciar.addEventListener("click", () => {
-      let section1 = document.createElement("div");
   
   //Primer pantalla de selección
       section1.innerHTML = `<div class="container py-5"></div>
@@ -101,9 +68,7 @@
   
   document.body.appendChild(section1);
   //Remover HTML para dar paso a la siguiente sección 
-  iniciar.remove();
-  mainContainer.remove();
-  
+ 
   document.querySelector("#infoOne");
   infoOne.addEventListener("click", () => {
       
@@ -144,66 +109,7 @@
   }
   )
   
-  function sectionTwo() {
-      let section2 = document.createElement("div");
   
-  //Primer pantalla de selección
-      section2.innerHTML = `<div class="container py-5 id="prueba"></div>
-      <div class="row justify-content-around text-center">
-        <div class="col-4 selectionCard">
-          <img src="src/android.svg" id="android" alt="">
-          <h1>Android</h1>
-        </div>
-        <div class="col-4 selectionCard" >
-          <img src="src/ios.svg" id="ios" alt="">
-            <h1>iOS</h1>
-        </div>
-      </div>
-  
-      <div class="container text-center py-5">
-          <button class="buttonInit btn btn-primary" id="infoOne">
-            Diccionario
-          </button>
-      </div>
-  
-      <template id="templateOne">
-          <swal-html>
-              <h5>NATIVA</h5>
-              <p>Es cuando se usa el lenguaje de programación propio del sistema operativo por ejemplo: Android permite Kotlin y Java mientras que iOS permite Swift.</p>
-              <h5>HIBRIDA</h5>
-              <p>Se usa una libreria como react native basada en tecnologia web que comparten los sistemas operativos para usar un solo codigo fuente y que la app funcione en ambos S.O</p>
-              <h5>WEBAPP</h5>
-              <p>Se usan librerias como React, Vue o Angular para crear una aplicación web que corra desde el navegador con funciones similares a una app instalable</p>
-          </swal-html>
-      </template>
-  `
-  
-  document.body.appendChild(section2);
-  
-  
-  
-  let selecion4 = document.querySelector("#android");
-  
-  selecion4.addEventListener("click", () => {
-      arrayVacio.push(sistemaOperativoUno)
-      section2.remove();
-      console.log(arrayVacio);
-      finalizar();
-  }
-  )
-  
-  let selecion5 = document.querySelector("#ios");
-  
-  selecion5.addEventListener("click", () => {
-      arrayVacio.push(sistemaOperativoDos)
-      section2.remove();
-      console.log(arrayVacio);
-      finalizar();
-  }
-  )
-  
-  
-  }//cierre de funcion two
   
   
   function finalizar() {
